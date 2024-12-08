8c196acb5295b7e7729acbf3dd0d0d29c5a4b496
RM550948 <rm550948@fiap.com.BR>
2024-12-08 14:40:37 -0300

    Add metodo GET

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify, request
+from infra.DB import Sleep
 
 app = Flask("__name__")
 
 @app.route('/dormir', methods=['POST'])
 def post_dormir():
+    sleep = request.json
+    Sleep.append(sleep)
+    return make_response(jsonify(Sleep))
+
+@app.route('/resultados_dormir', methods=['GET'])
+def get_sleep():
+    return make_response(
+        jsonify(Sleep=Sleep,
+        )
+    )
 
 app.run()
