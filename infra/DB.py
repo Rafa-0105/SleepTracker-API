c47dac96967507ff2223b7e8151c91a6ab9557cd
RM550948 <rm550948@fiap.com.BR>
2024-12-08 14:40:01 -0300

    Dados mocados no DB.

--- infra/DB.py
+++ infra/DB.py
@@ -1,19 +1,30 @@
-import os
-from dotenv import load_dotenv
-import mysql.connector
-
-load_dotenv()
-
-conexao = mysql.connector.connect(
-    host=os.getenv("DATABASE_HOST"),
-    user=os.getenv("DATABASE_USER"),
-    password=os.getenv("DATABASE_PASSWORD"),
-    database=os.getenv("DATABASE_NAME")
-)
-
-cursor = conexao.cursor()
-
-cursor.execute("select @@version")
-version = cursor.fetchone()
-if version:
-    print('Conectado com sucesso')
+Sleep = [
+    {
+        "id": 1,
+        "dia_dormir": "08/12",
+        "hora_dormir": "20:30",
+        "dia_acordar": "09/12",
+        "hora_acordar": "05:00"
+    },
+    {
+        "id": 2,
+        "dia_dormir": "28/05",
+        "hora_dormir": "19:30",
+        "dia_acordar": "29/05",
+        "hora_acordar": "06:30"
+    },
+    {
+        "id": 3,
+        "dia_dormir": "12/12",
+        "hora_dormir": "23:30",
+        "dia_acordar": "13/12",
+        "hora_acordar": "07:00"
+    },
+    {
+        "id": 4,
+        "dia_dormir": "31/12",
+        "hora_dormir": "01:30",
+        "dia_acordar": "31/12",
+        "hora_acordar": "10:00"
+    }
+]
